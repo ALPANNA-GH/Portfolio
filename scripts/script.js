71bb9e18164b5cccf36340f97c65140c0bc4e403
Ahmed Alpanna <alpanna@gmail.com>
2023-07-26 15:41:52 +0200

    generate mywork section (projects) dynamicaly using js array, objects and loop

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,20 @@ window.onload = function () {
   const menueItem = document.querySelectorAll('.navitem');
   const projects = [
     {
+      proName: 'Multi-Post Stories',
+      proDesc: `A daily selection of privately personalized reads; 
+                no accounts or sign-ups required. has been the industry's 
+                standard dummy text ever since the 1500s, 
+                when an unknown printer took a standard dummy text.`,
+      proImage: '../images/placeholder.svg',
+      proTechs: ['CSS', 'HTML', 'Boostrab', 'Ruby'],
+      liveLink: 'http://www.github.com/proj1',
+      sourceLink: 'http://www.proj1.com',
+    },
+    {
       proName: 'Profesional Art Printing Data More',
-      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
-                + " Has been the industry's standard.",
+      proDesc: `A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                Has been the industry's standard.`,
       proImage: '../images/bgPlaceholder.svg',
       proTechs: ['HTML', 'Boostrab', 'Ruby'],
       liveLink: 'http://www.github.com/proj1',
@@ -15,8 +26,8 @@ window.onload = function () {
     },
     {
       proName: 'Data Dashboard Healthcare',
-      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
-              + " Has been the industry's standard.",
+      proDesc: `A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                Has been the industry's standard.`,
       proImage: '../images/bgPlaceholder1.png',
       proTechs: ['HTML', 'Boostrab', 'Ruby'],
       liveLink: 'http://www.proj1.com',
@@ -24,8 +35,8 @@ window.onload = function () {
     },
     {
       proName: 'Website Portfolio',
-      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
-              + " Has been the industry's standard.",
+      proDesc: `A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                Has been the industry's standard.`,
       proImage: '../images/bgPlaceholder2.png',
       proTechs: ['HTML', 'Boostrab', 'Ruby'],
       liveLink: 'http://www.proj1.com',
@@ -33,8 +44,8 @@ window.onload = function () {
     },
     {
       proName: 'Profesional Art Printing Data More',
-      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
-                + " Has been the industry's standard.",
+      proDesc: `A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                + " Has been the industry's standard.`,
       proImage: '../images/bgPlaceholder.svg',
       proTechs: ['HTML', 'Boostrab', 'Ruby'],
       liveLink: 'http://www.github.com/proj1',
@@ -42,8 +53,8 @@ window.onload = function () {
     },
     {
       proName: 'Data Dashboard Healthcare',
-      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
-              + " Has been the industry's standard.",
+      proDesc: `A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                Has been the industry's standard.`,
       proImage: '../images/bgPlaceholder1.png',
       proTechs: ['HTML', 'Boostrab', 'Ruby'],
       liveLink: 'http://www.proj1.com',
@@ -51,14 +62,63 @@ window.onload = function () {
     },
     {
       proName: 'Website Portfolio',
-      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
-              + " Has been the industry's standard.",
+      proDesc: `A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                Has been the industry's standard.`,
       proImage: '../images/bgPlaceholder2.png',
       proTechs: ['HTML', 'Boostrab', 'Ruby'],
       liveLink: 'http://www.proj1.com',
       sourceLink: 'http://www.proj1.com',
     },
-];
+  ];
+  
+  let techs = '';
+  for (let i = 0; i < projects[0].proTechs.length; i++) {
+    techs += `<li><a href="#"> ${projects[0].proTechs[i]}</a></li>`;
+  }
+
+  let sectionHTML = `<div class="item recent-work">
+      <h2 class="maintitle"> My Recent Work</h2>
+      <img class="seperator" src="./images/lineseperator.png" />
+    </div>
+    <div class="item multi-post">
+      <div class="mp-img">
+        <img src="${projects[0].proImage}" alt="placeholder" />
+      </div>
+      <div class="mp-txt">
+        <h2 class="secondry">${projects[0].proName}</h2>
+        <p>
+        ${projects[0].proDesc}
+        </p>
+        <ul class="mp-list">
+        ${techs}
+        </ul>
+        <button class="buttn" type="button">See project</button>
+      </div>
+    </div>`;
+
+  for (let i = 1; i < projects.length; i++) {
+    techs = '';
+    for (let t = 0; t < projects[i].proTechs.length; t++) {
+      techs += `<li><a href="#"> ${projects[i].proTechs[t]}</a></li>`;
+    }
+
+    sectionHTML += `
+      <div class="item bgmask" style="background-image: url('${projects[i].proImage}')">
+        <div>
+          <h2 class="secondry">${projects[i].proName}</h2>
+          <p>
+            ${projects[i].proDesc}
+          </p>
+          <ul>
+            ${techs}
+          </ul>
+          <button class="butn" type="button">See project</button>
+        </div>
+      </div>
+      `;
+  }
+
+  document.getElementById('mywork').innerHTML = sectionHTML;
 
   function showMenue() {
     menueNavBar.classList.replace('nav', 'nav-open');
