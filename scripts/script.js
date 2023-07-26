a5591806cda3d36119e356061414630a8de2d0d9
Ahmed Alpanna <alpanna@gmail.com>
2023-07-26 08:41:24 +0200

    created array of objects

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,62 @@ window.onload = function () {
   const menueCancelBtn = document.querySelector('#menueCancle');
   const menueNavBar = document.querySelector('#navbar');
   const menueItem = document.querySelectorAll('.navitem');
+  const projects = [
+    {
+      proName: 'Profesional Art Printing Data More',
+      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                + " Has been the industry's standard.",
+      proImage: '../images/bgPlaceholder.svg',
+      proTechs: ['HTML', 'Boostrab', 'Ruby'],
+      liveLink: 'http://www.github.com/proj1',
+      sourceLink: 'http://www.proj1.com',
+    },
+    {
+      proName: 'Data Dashboard Healthcare',
+      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
+              + " Has been the industry's standard.",
+      proImage: '../images/bgPlaceholder1.png',
+      proTechs: ['HTML', 'Boostrab', 'Ruby'],
+      liveLink: 'http://www.proj1.com',
+      sourceLink: 'http://www.proj1.com',
+    },
+    {
+      proName: 'Website Portfolio',
+      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
+              + " Has been the industry's standard.",
+      proImage: '../images/bgPlaceholder2.png',
+      proTechs: ['HTML', 'Boostrab', 'Ruby'],
+      liveLink: 'http://www.proj1.com',
+      sourceLink: 'http://www.proj1.com',
+    },
+    {
+      proName: 'Profesional Art Printing Data More',
+      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
+                + " Has been the industry's standard.",
+      proImage: '../images/bgPlaceholder.svg',
+      proTechs: ['HTML', 'Boostrab', 'Ruby'],
+      liveLink: 'http://www.github.com/proj1',
+      sourceLink: 'http://www.proj1.com',
+    },
+    {
+      proName: 'Data Dashboard Healthcare',
+      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
+              + " Has been the industry's standard.",
+      proImage: '../images/bgPlaceholder1.png',
+      proTechs: ['HTML', 'Boostrab', 'Ruby'],
+      liveLink: 'http://www.proj1.com',
+      sourceLink: 'http://www.proj1.com',
+    },
+    {
+      proName: 'Website Portfolio',
+      proDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
+              + " Has been the industry's standard.",
+      proImage: '../images/bgPlaceholder2.png',
+      proTechs: ['HTML', 'Boostrab', 'Ruby'],
+      liveLink: 'http://www.proj1.com',
+      sourceLink: 'http://www.proj1.com',
+    },
+];
 
   function showMenue() {
     menueNavBar.classList.replace('nav', 'nav-open');
